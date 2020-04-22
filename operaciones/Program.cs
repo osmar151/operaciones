aa2226a71317410c4fdbb063af3a0ac85c2504ef
osmar151 <62614167+osmar151@users.noreply.github.com>
2020-04-22 07:29:02 -0600

    primera parte hasta donde piden los valores

---
+++ operaciones/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace operaciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int op = 0;
+            char menu = 'n';
+            double n1 = 0;
+            double n2 = 0;
+            double total = 0;
+            string linea = "";
+
+            while (menu!= 's')
+            {
+                Console.WriteLine("1.-suma");
+                Console.WriteLine("2.-resta");
+                Console.WriteLine("3.-multiplicacion");
+                Console.WriteLine("4.-division");
+                Console.WriteLine("5.-salir");
+                Console.WriteLine("opcion: ");
+
+                linea = Console.ReadLine();
+                op = int.Parse(linea);
+
+
+                Console.WriteLine("ingrese el primer valor: ");
+                linea = Console.ReadLine();
+                n1 = int.Parse(linea);
+
+                Console.WriteLine("ingrese el segundo valor: ");
+                linea = Console.ReadLine();
+                n2 = int.Parse(linea);
+
+               
+            }
+
+
+
+
+
+        }
+    }
+}
